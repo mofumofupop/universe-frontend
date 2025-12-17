c33c21a55d5b4224c8d902a5894c336c1e559aca
usamimi39 <usamin04tp@gmail.com>
2025-12-17 20:02:04 +0900

    add-link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 export default function Home () {
     return (
-        <div>Welcome to Universe</div>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1>Welcome to Universe</h1>
+            <Link href="/sparkle">
+                <button className="bg-slate-400 text-white p-2 rounded-lg shadow-md">
+                    Log in
+                </button>
+            </Link>
+        </div>
     );
 }
