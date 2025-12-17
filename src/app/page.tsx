d03b11bdc5cad66da12fc101b7eef3e51aac000a
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-12-17 21:44:58 +0900

    Add Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import Header from '@/components/Header';
 
 export default function Home () {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
+            <Header className="text-white" />
             <h1>Welcome to Universe</h1>
             <Link href="/sparkle">
                 <button className="bg-slate-400 text-white p-2 rounded-lg shadow-md">
