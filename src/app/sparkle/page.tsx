d53033a63d6d48b97771b9335dee09e499ad3517
usamimi39 <usamin04tp@gmail.com>
2025-12-19 13:48:36 +0900

    few-change

--- src/app/sparkle/page.tsx
+++ src/app/sparkle/page.tsx
@@ -1,7 +1,5 @@
 import { User } from "@/types/User";
-import { CardFront } from "@/components/CardFront";
-import { CardBack } from "@/components/CardBack";
-import FlipAnimation from "@/components/FlipAnimation";
+import FlipAnimation from "@/components/Card/FlipAnimation";
 
 export default function Sparkle() {
   // デモデータ
@@ -10,27 +8,21 @@ export default function Sparkle() {
     username: "onnenai_w57",
     name: "Onnenai",
     affiliation: "Project Manager / Frontend\nEngineer / Composer / DJ",
-  
-    icon_url: "https://links.onnenai.cc/images/icon.png", 
+
+    icon_url: "https://links.onnenai.cc/images/icon.png",
     social_links: [
       "https://x.com/onnenai_w57",
       "https://instagram.com/onnenai_w57",
       "https://discord.gg/",
       "https://github.com/usamimi39",
-      "https://blogs.onnenai.cc"
-    ]
+      "https://blogs.onnenai.cc",
+    ],
   };
 
   return (
     <div className="flex items-center flex-col mt-8">
       <div className="w-full p-4 gap-4">
         <FlipAnimation user={demoUser} />
-        {/* <div>
-          <CardFront user={demoUser} />
-        </div>
-        <div>
-          <CardBack user={demoUser} />
-        </div> */}
       </div>
     </div>
   );
